6cd287373030be3c49d976ffa178cb484f57a0c9
marwhals <marjan.mubarok2012@gmail.com>
2024-01-08 20:59:31 +0000

    IO Apps

--- src/main/scala/part2effects/IOApps.scala
+++ src/main/scala/part2effects/IOApps.scala
@@ -1,5 +1,35 @@
 package part2effects
 
-object IOApps {
+import cats.effect.{ExitCode, IO, IOApp}
 
+import scala.io.StdIn
+
+object IOApps {
+  val program = for {
+    line <- IO(StdIn.readLine())
+    _ <- IO(println(s"You've just written: $line"))
+  } yield ()
+}
+
+object TestApp {
+
+  def main(args: Array[String]): Unit = {
+    import IOApps.program
+
+    import cats.effect.unsafe.implicits.global
+    program.unsafeRunSync()
+  }
+}
+
+object FirstCEApp extends IOApp {
+  import IOApps._
+
+  override def run(args: List[String]) =
+    program.as(ExitCode.Success) // Return exit code wrapped in IO
+}
+
+object MySimpleApp extends IOApp.Simple {
+  import IOApps._
+
+  override def run = program
 }
